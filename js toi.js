38f64053e412e02d709ef718c040eb83ffd90438
Ayanna Rose Banks <113248600+arbanks77@users.noreply.github.com>
2023-01-30 23:44:30 -0500

    Add files via upload

--- js toi.js
+++ js toi.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+    });
